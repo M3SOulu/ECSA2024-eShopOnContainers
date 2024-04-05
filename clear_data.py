7440423428fe21971a2834e8a077e2f7c91cb737
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-04-05 15:56:50 +0300

    clean: remove duplicate emails

---
+++ clear_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import json
+
+
+def clear_none(f):
+    df = pd.read_csv(f)
+    df = df[df.developer_a != '(none)']
+    df = df[df.developer_b != '(none)']
+    df.to_csv(f, index=False, header=True)
+
+
+def clear_email(f):
+    with open('email_map.json', 'r') as fi:
+        d = json.load(fi)
+    df = pd.read_csv(f)
+    df['developer_a'] = df['developer_a'].replace(d)
+    df['developer_b'] = df['developer_b'].replace(d)
+    df.to_csv(f, index=False, header=True)
