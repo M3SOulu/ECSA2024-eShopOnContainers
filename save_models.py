1f3b8e23362ae8982829f401fc4bb023aa7b0a43
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-04-11 18:42:40 +0300

    add: save the discovered communities

---
+++ save_models.py
@@ -0,0 +1,19 @@
+from tenetan.networks import SnapshotGraph
+from tenetan.community import community_parafac_nn_als
+
+import pickle
+
+file_net = SnapshotGraph("data_file_network_concat_network.csv")
+service_net = SnapshotGraph("data_service_network_concat_network.csv")
+
+file_comm = community_parafac_nn_als(file_net, 4,
+                                     "in_communities", "out_communities", "raw_temporal_activity",
+                                     "in_temporal_activity", "out_temporal_activity", "core_consistency", init='svd')
+service_comm = community_parafac_nn_als(service_net, 4,
+                                        "in_communities", "out_communities", "raw_temporal_activity",
+                                        "in_temporal_activity", "out_temporal_activity", "core_consistency", init='svd')
+
+with open("file_communities.pkl", 'wb') as f:
+    pickle.dump(file_comm, f)
+with open("service_communities.pkl", 'wb') as f:
+    pickle.dump(service_comm, f)
