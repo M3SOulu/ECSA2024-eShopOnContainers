b8e852cadb756f56b9e83c4737a4d555c4b673b7
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-04-23 15:56:04 +0300

    add: normalize temporal activity

--- temporal_activity.py
+++ temporal_activity.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pickle
 
 with open('file_communities.pkl', 'rb') as f:
@@ -9,24 +10,33 @@ with open('service_communities.pkl', 'rb') as f:
 xticks = ['2.0.0', '2.0.5', '2.0.8', '2.2.0', '3.0.0']
 fig, axs = plt.subplots(2, 2)
 
+file_raw = file_comms['raw_temporal_activity']
+file_in = file_comms['in_temporal_activity']
+service_raw = service_comms['raw_temporal_activity']
+service_in = service_comms['in_temporal_activity']
+file_in = file_in / np.max(file_in)
+file_raw = file_raw / np.max(file_raw)
+service_in = service_in / np.max(service_in)
+service_raw = service_raw / np.max(service_raw)
+
 plt.subplot(221)
-plt.plot(file_comms['raw_temporal_activity'])
+plt.plot(file_raw)
 plt.title('File collaboration')
 plt.ylabel('Raw activity')
 plt.xticks([0, 1, 2, 3, 4], xticks)
 
 plt.subplot(222)
-plt.plot(service_comms['raw_temporal_activity'])
+plt.plot(service_raw)
 plt.title('Service collaboration')
 plt.xticks([0, 1, 2, 3, 4], xticks)
 
 plt.subplot(223)
-plt.plot(file_comms['in_temporal_activity'])
+plt.plot(file_in)
 plt.ylabel('Scaled activity')
 plt.xticks([0, 1, 2, 3, 4], xticks)
 
 plt.subplot(224)
-plt.plot(service_comms['in_temporal_activity'])
+plt.plot(service_in)
 plt.xticks([0, 1, 2, 3, 4], xticks)
 
 fig.supxlabel('Release')
