607b043fdf43913d6c32bc3bddebd6f3a0a90162
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-04-26 11:31:56 +0300

    add: arrange plot horizontally

--- temporal_activity.py
+++ temporal_activity.py
@@ -8,23 +8,22 @@ with open('service_communities.pkl', 'rb') as f:
     service_comms = pickle.load(f)
 
 xticks = ['2.0.0', '2.0.5', '2.0.8', '2.2.0', '3.0.0']
-fig, axs = plt.subplots(2, 1)
 
 file_raw = file_comms['raw_temporal_activity']
 service_raw = service_comms['raw_temporal_activity']
 file_raw = file_raw / np.max(file_raw)
 service_raw = service_raw / np.max(service_raw)
 
-plt.subplot(211)
+fig, axes = plt.subplots(1,2, figsize=(7,4))
+plt.subplot(121)
 plt.plot(file_raw[:,0], 'o-', label='File comm. 1')
 plt.plot(file_raw[:,1], 'x--', label='File comm. 2')
 plt.title('File collaboration')
-#plt.xticks([0, 1, 2, 3, 4], xticks)
-ax = plt.gca()
-ax.get_xaxis().set_visible(False)
+plt.xticks([0, 1, 2, 3, 4], xticks)
+plt.ylabel('Normalized activity')
 #plt.legend()
 
-plt.subplot(212)
+plt.subplot(122)
 plt.plot(service_raw[:,0], 'o-', label='Service comm. 1')
 plt.plot(service_raw[:,1], 'x--', label='Service comm. 2')
 plt.plot(service_raw[:,2], '+-.', label='Service comm. 3')
